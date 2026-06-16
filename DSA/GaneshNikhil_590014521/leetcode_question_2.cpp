972e80184a90e2012218c5603b9967d7284b0742
GANESH NIKHIL <ganeshnikhil124@gmail.com>
2026-06-17 00:21:53 +0530

    Add leetcode question 2

---
+++ DSA/GaneshNikhil_590014521/leetcode_question_2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        int j = nums.size() - 1;
+        while(i <= j){
+            if(nums[i] == val){
+                nums[i] = nums[j];
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return j+1;
+    }
+};
