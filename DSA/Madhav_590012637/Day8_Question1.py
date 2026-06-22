233bbd383c5fcf55b6bb2f8248493e379870d5a4
Madhav Semwal <Madhav.12637@stu.upes.ac.in>
2026-06-22 11:54:18 +0530

    Delete DSA/Madhav_590012637/Day8_Question1.py

--- DSA/Madhav_590012637/Day8_Question1.py
+++
@@ -1,8 +0,0 @@
-def isPalindrome(s):
-    
-    filtered = ''.join(c.lower() for c in s if c.isalnum())
-    return filtered == filtered[::-1]
-
-print(isPalindrome("A man, a plan, a canal: Panama"))  # true
-print(isPalindrome("race a car"))                       # false
-print(isPalindrome(" "))                                # true
