818b0a4add6894aaf640406ff3d9b35267c2af7c
ManuDurga <you@example.com>
2026-08-23 21:54:12 +0530

    Week Sunday Debug Submission

---
+++ Sunday_Debug/Sunday_debug_10/Question.py
@@ -0,0 +1,44 @@
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def insert(root, value):
+
+    # Hulk recovered these lines from different files.
+    # Their order may not be correct.
+    if root is None:
+            return Node(value)
+    
+    if value < root.value:
+            root.left = insert(root.left, value)
+    elif value > root.value:
+        root.right = insert(root.right, value)
+
+    return root
+
+def search(root, target):
+
+    # Hulk may have moved some lines here too.
+    if root is None:
+            return False
+        
+    if root.value == target:
+        return True
+    elif target < root.value:
+        return search(root.left, target)
+    else:
+        return search(root.right, target)
+
+
+root = None
+
+gamma_readings = [50, 30, 70, 20, 40, 60, 80]
+
+for reading in gamma_readings:
+    root = insert(root, reading)
+
+print(search(root, 60))
+print(search(root, 25))
